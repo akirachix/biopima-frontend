794231025da3b6c85f1ab2947475b1a7be7a0d82
sharonmuriithi <sharonnyaguthiimuriithi@gmail.com>
2025-09-15 16:39:56 +0300

    Created the sidebar component

--- biopima/src/app/page.tsx
+++ biopima/src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Sidebar from "./shared-components/sidebar/institution/page";
+import Sidebar from "./shared-components/sidebar/institution";
 export default function Home() {
   return (
     <div >
