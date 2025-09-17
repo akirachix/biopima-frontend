ff09ed8a7e66290fa9aecd8f83b26da8936560fa
mildredosebe <mildredosebeochoki@gmail.com>
2025-09-17 15:00:42 +0300

    created institution and consulatant sidebar

--- biopima/src/app/page.tsx
+++ biopima/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      <h2>BioPima dashboard</h2>
-      
-    </div>
+  
+    
+<div>
+</div>
+  
   );
 }
