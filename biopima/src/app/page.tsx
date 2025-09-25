0abbd41c9fb0b75c9d8c835c01710bfd9515494d
Mercyline Makuto <makutomercyline@gmail.com>
2025-09-25 16:33:23 +0300

    added the consultant dashboard screen

--- biopima/src/app/page.tsx
+++ biopima/src/app/page.tsx
@@ -1,10 +1,8 @@
 
-import DashboardScreen from "./dashboard/page";
 import BioPimaTeaser from "./teaser-screen/page";
 export default function Home() {
   return (
     <div >
-      <DashboardScreen/>
       <BioPimaTeaser/>
 
     </div>
