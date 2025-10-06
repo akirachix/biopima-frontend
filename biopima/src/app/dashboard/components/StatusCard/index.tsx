531b5478186abf2912511470f335332de74ae846
Marion <marionnyaisanga@gmail.com>
2025-10-06 14:19:38 +0300

    updated live readings

--- biopima/src/app/dashboard/components/StatusCard/index.tsx
+++ biopima/src/app/dashboard/components/StatusCard/index.tsx
@@ -81,7 +81,7 @@ export default function StatusCard({ type, latestReading }: StatusCardProps) {
 
 
  return (
-   <div className="flex flex-col rounded-xl p-6 bg-[#a9f5d0] max-w-xs shadow-md">
+   <div className="flex flex-col rounded-xl p-6 ml-[20px] bg-[#a9f5d0] max-w-xs shadow-md">
      <div className="flex items-center">
        <div className="rounded-full bg-green-300 p-2 mr-3">
          <Activity className="w-6 h-6 text-green-800" />
--- biopima/src/app/dashboard/page.tsx
+++ biopima/src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+
+
 import React from 'react';
 import useLiveSensorReadings from '../hooks/useFetchSensorReadings';
 import MetricCard from './components/MetricCard';
